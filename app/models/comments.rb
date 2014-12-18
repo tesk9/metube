fa12e39a0a9e683d07f28e33e64d1177a409c8e0
Tessa <tessakkelly@gmail.com>
2014-12-18 10:37:40 -0800

    Make required input fields with specified lengths

--- app/models/comments.rb
+++ app/models/comments.rb
@@ -1,3 +1,4 @@
 class Comments < ActiveRecord::Base
   belongs_to :video
+  validates :subject, length: {maximum: 50}, presence: true
 end
--- app/models/videos.rb
+++ app/models/videos.rb
@@ -1,3 +1,5 @@
 class Videos < ActiveRecord::Base
   has_many :comments
+  validates :title, uniqueness: true, length: {minimum: 2, maximum: 140}, presence: true
+  validates :url, uniqueness: true, presence: true
 end
